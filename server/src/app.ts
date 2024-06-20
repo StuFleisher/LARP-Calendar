12285568293286f86410a1f5fcf6469dfd25fe05
Stuart Fleisher <stufleisher@gmail.com>
2024-06-20 13:43:38 -0400

    Added routing for larps and users

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,33 +3,32 @@ import express, { ErrorRequestHandler, Request, Response, NextFunction } from "e
 import cors from 'cors';
 import { NotFoundError } from './utils/expressError';
 // import { authenticateJWT } from "./middleware/auth";
+import larpRoutes from './routes/larps';
+import usersRoutes from './routes/users';
 
-const app=express();
+const app = express();
 
-const corsOptions={
-  origin:process.env.CORS_URL,
+const corsOptions = {
+  origin: process.env.CORS_URL,
   optionSuccessStatus: 200
-}
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
 // app.use(authenticateJWT);
 
 
-// app.use("/auth", authRoutes);
-// app.use("/recipes", recipesRoutes);
-// app.use("/users", usersRoutes);
-// app.use("/bugReports", bugReportRoutes);
-
+app.use("/events", larpRoutes);
+app.use("/users", usersRoutes);
 
 /** Handle 404 errors -- this matches everything */
-app.use(function (req:Request, res:Response, next:NextFunction) {
+app.use(function (req: Request, res: Response, next: NextFunction) {
   throw new NotFoundError();
 });
 
 
 /** Generic error handler; anything unhandled goes here. */
-const genericErrorHandler:ErrorRequestHandler = (err,req,res,next) =>{
+const genericErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   if (process.env.NODE_ENV !== "test") console.error(err.stack);
 
   let status = err.status || 500;
@@ -38,7 +37,7 @@ const genericErrorHandler:ErrorRequestHandler = (err,req,res,next) =>{
   return res.status(status).json({
     error: { message, status },
   });
-}
+};
 app.use(genericErrorHandler);
 
 export default app;
