141685b4891867ffcddc8b7344d24138670ccfa4
Stuart Fleisher <stufleisher@gmail.com>
2024-06-28 16:19:51 -0400

    Added more types

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -2,22 +2,22 @@
 export type Tag = {
   id: number;
   name: string;
-}
+};
 
 export enum ticketStatus {
-  AVAILABLE="Available",
-  LIMITED="Limited",
-  SOLD_OUT="Sold-Out"
+  AVAILABLE = "Available",
+  LIMITED = "Limited",
+  SOLD_OUT = "Sold-Out"
 }
 
 export type Larp = {
-  id: number
+  id: number;
   title: string,
   ticketStatus: ticketStatus,
   tags: Tag[],
   start: Date,
   end: Date,
-  allDay:boolean,
+  allDay: boolean,
   imgUrl: string,
   city: string,
   country: string,
@@ -25,5 +25,23 @@ export type Larp = {
   description: string,
   organizer: string,
   eventUrl: string,
-}
+};
 
+export type UserForCreate = {
+  username: string,
+  password: string,
+  firstName: string,
+  lastName: string,
+  email: string,
+  isOrganizer: boolean,
+  isAdmin: boolean,
+};
+
+export type User = UserForCreate & {
+  id: number;
+};
+
+export type UserLoginData = {
+  username: string,
+  password: string,
+};
