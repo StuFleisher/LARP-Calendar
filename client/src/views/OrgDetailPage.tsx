93c8fa5a569867f2c4a7568a2879f7ab8200c840
Stuart Fleisher <stufleisher@gmail.com>
2024-08-01 13:33:16 -0400

    Added alert for unapproved organizations

--- client/src/views/OrgDetailPage.tsx
+++ client/src/views/OrgDetailPage.tsx
@@ -3,7 +3,10 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useFetchOrg } from "../hooks/useFetchOrg";
 import LoadingSpinner from "../components/ui/LoadingSpinner";
 import OrgDetails from "../components/Orgs/OrgDetails";
-
+import { Alert } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "@mui/material";
 
 
 
@@ -26,7 +29,14 @@ function OrgDetailPage() {
     if (loading) return (<LoadingSpinner />);
 
     if (org) return (
-        <OrgDetails org={org}/>
+        <>
+            {!org.isApproved &&
+                <Alert severity="success" icon={<FontAwesomeIcon icon={faCheck} />}>
+                    Your application is currently being reviewed by our admin team. Once your application has been approved you will be able to publish events. Send questions to <Link to="mailto:info@larpcalendar.com">info@larpcalendar.com</Link>
+                </Alert>
+            }
+            <OrgDetails org={org} />
+        </>
     );
 
 }
