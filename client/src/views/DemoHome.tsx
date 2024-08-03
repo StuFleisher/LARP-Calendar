e85dd3679b546b94a18d94210b6f452964cbd957
Stuart Fleisher <stufleisher@gmail.com>
2024-08-03 12:20:34 -0400

    Added autologin endpoint for demo

---
+++ client/src/views/DemoHome.tsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+import LarpAPI from "../util/api";
+import LoadingSpinner from "../components/ui/LoadingSpinner";
+import { Box, Button, List, ListItem, Stack, Typography } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faCheck, faCheckCircle, faCircleChevronRight, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { Link as RouterLink } from "react-router-dom";
+
+function DemoHome() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loginDemoUser() {
+      await LarpAPI.userLogin({ username: "testUser", password: "password" });
+      setLoading(false);
+    }
+    loginDemoUser();
+  }, [setLoading]);
+
+  return (
+    <>
+      {
+        loading
+          ?
+          <>
+            <Typography variant="h3"> Logging you in </Typography>
+            <LoadingSpinner />
+          </>
+          :
+          <>
+            <Stack
+              direction="column"
+              spacing={2}
+              alignContent="center"
+              m="2rem"
+            >
+              <Box className="filled-light"
+                sx={{
+                  width: '100%',
+                  textAlign: 'center',
+                  padding: '3rem'
+                }}
+              >
+
+                <Typography component="h1" variant='h1' className="title">
+                  <Typography
+                    variant='h1'
+                    component='span'
+                    sx={{ display: 'inline' }}
+                    color='primary'
+                  >
+                    <FontAwesomeIcon icon={faCheckCircle} />  &nbsp;
+                  </Typography>
+                  Welcome to the LarpCal Demo!
+                </Typography>
+                <Typography variant="h4">
+                  You have been logged in as a Demo User with admin privileges.
+                </Typography>
+              </Box>
+
+              <Typography variant="h5">
+                This site is still under construction.  Here are some of the features you can expect to see in Phase II:
+              </Typography>
+              <List dense>
+                <ListItem>
+                  <FontAwesomeIcon icon={faArrowRight} /> &nbsp; Fulltext search for events
+                </ListItem>
+                <ListItem>
+                  <FontAwesomeIcon icon={faArrowRight} /> &nbsp;
+                  Event List page will be filterable by date, location, tags and more
+                </ListItem>
+                <ListItem>
+                  <FontAwesomeIcon icon={faArrowRight} /> &nbsp;
+                  {`Category displays (featured, this month etc) will display only the relevant events`}
+                </ListItem>
+                <ListItem>
+                  <FontAwesomeIcon icon={faArrowRight} /> &nbsp;
+                  Connection to donation site
+                </ListItem>
+                <ListItem>
+                  <FontAwesomeIcon icon={faArrowRight} /> &nbsp;
+                  Full page build for 'About'
+                </ListItem>
+                <ListItem>
+                  <FontAwesomeIcon icon={faArrowRight} /> &nbsp;
+                  Ongoing bugfixes
+                </ListItem>
+              </List>
+
+              <Button variant="contained" component={RouterLink} to="/">Go to Home Page</Button>
+
+            </Stack>
+
+          </>
+      }
+
+
+
+
+
+
+    </>
+
+  );
+
+}
+
+export default DemoHome;
