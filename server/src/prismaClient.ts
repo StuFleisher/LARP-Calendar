1b96a3f87b072e447b69b7e00b3c911af6fc84f0
Stuart Fleisher <stufleisher@gmail.com>
2024-12-07 09:54:49 -0500

    Added logging for prod debug issue

--- server/src/prismaClient.ts
+++ server/src/prismaClient.ts
@@ -4,5 +4,7 @@
 
 import { PrismaClient} from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+})
 export default prisma
